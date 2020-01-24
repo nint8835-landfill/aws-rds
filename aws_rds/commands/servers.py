be648adf5994d768eac97064646af653738e8fa6
Riley Flynn <riley@colabsoftware.com>
2020-01-24 19:20:32 -0330

    Added server adding

--- aws_rds/commands/servers.py
+++ aws_rds/commands/servers.py
@@ -1,12 +1,59 @@
+import sys
+from enum import Enum
+
 import click
 
-from ..servers import ServerList
+from ..servers import Server, ServerList, ServerRegion
 
 
 @click.group()
 def servers():
     """Manage the saved server list."""
     pass
+
+
+@servers.command()
+@click.option(
+    "--name", prompt=True, help="Alias used to refer to the server in all commands."
+)
+@click.option("--hostname", prompt=True, help="Hostname of the RDS instance to add.")
+@click.option(
+    "--port", prompt=True, default=5432, help="Port of the RDS instance to add."
+)
+@click.option(
+    "--region",
+    prompt=True,
+    help="Region the RDS instance to add is located in.",
+    type=click.Choice([i.replace("_", "-") for i in ServerRegion.__members__]),
+    callback=lambda c, p, v: getattr(ServerRegion, v.replace("-", "_")) if v else None,
+)
+@click.option(
+    "--username", prompt=True, help="Username to use to connect to the RDS instance."
+)
+@click.option(
+    "--profile",
+    prompt=True,
+    help="Okta profile to use to generate a token for the RDS instance.",
+)
+def add(name, hostname, port, region, username, profile):
+    """Add a new server to the server list."""
+    server_list = ServerList.load()
+    for server in server_list.servers:
+        if server.name == name:
+            click.secho(f"A server with the name {name} already exists.", fg="red")
+            sys.exit(1)
+    server_list.servers.append(
+        Server(
+            name=name,
+            hostname=hostname,
+            port=port,
+            region=region,
+            username=username,
+            profile=profile,
+        )
+    )
+    server_list.save()
+    click.secho(f"Server {name} added to server list.", fg="green")
 
 
 @servers.command()
--- aws_rds/servers.py
+++ aws_rds/servers.py
@@ -1,9 +1,31 @@
 import json
 import pathlib
+from enum import Enum
 from typing import List
 
 import click
 from pydantic import BaseModel
+
+
+class ServerRegion(Enum):
+    us_east_1 = "us-east-1"
+    us_east_2 = "us-east-2"
+    us_west_1 = "us-west-1"
+    us_west_2 = "us-west-2"
+    ap_east_1 = "ap-east-1"
+    ap_south_1 = "ap-south-1"
+    ap_northeast_1 = "ap-northeast-1"
+    ap_northeast_2 = "ap-northeast-2"
+    ap_northeast_3 = "ap-northeast-3"
+    ap_southeast_1 = "ap-southeast-1"
+    ap_southeast_2 = "ap-southeast-2"
+    ca_central_1 = "ca-central-1"
+    eu_west_1 = "eu-west-1"
+    eu_west_2 = "eu-west-2"
+    eu_west_3 = "eu-west-3"
+    eu_north_1 = "eu-north-1"
+    me_south_1 = "me-south-1"
+    sa_east_1 = "sa-east-1"
 
 
 class Server(BaseModel):
@@ -11,7 +33,7 @@ class Server(BaseModel):
     profile: str
     hostname: str
     port: int
-    region: str
+    region: ServerRegion
     username: str
 
     def format(self) -> str:
@@ -21,7 +43,7 @@ class Server(BaseModel):
                 f"    {click.style('Profile:', fg='yellow')} {self.profile}",
                 f"    {click.style('Hostname:', fg='yellow')} {self.hostname}",
                 f"    {click.style('Port:', fg='yellow')} {self.port}",
-                f"    {click.style('Region:', fg='yellow')} {self.region}",
+                f"    {click.style('Region:', fg='yellow')} {self.region.value}",
                 f"    {click.style('Username:', fg='yellow')} {self.username}",
             ]
         )
